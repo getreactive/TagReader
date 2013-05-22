e9eababb458fcb3287dca19724cf289bcf3edca4
Rahul Kumar <rahulkumar.aws@gmail.com>
2013-05-23 00:36:50 +0530

    New TagReader project created.

---
+++ TagReader/src/tagreader/TagReader.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tagreader;
+
+/**
+ *
+ * @author rahul
+ */
+public class TagReader {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
